5ad5184591b2ef8e2692c91ab78dfc49fe8eb46c
ASUS <ASUS@LAPTOP-BSCKNOUH>
2022-10-29 11:23:28 +0630

    added AppController and home.html

---
+++ week2day1/src/main/java/com/software/week2/AppController.java
@@ -0,0 +1,16 @@
+package com.software.week2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+	
+	@GetMapping("/viewPage")
+	public String viewPage(Model model)
+	{	model.addAttribute("title", "Thymeleaf Exercise");
+		model.addAttribute("message", "Hi everyone! Passionate in Learning Spring boot app");
+		return "home";
+	}
+}
